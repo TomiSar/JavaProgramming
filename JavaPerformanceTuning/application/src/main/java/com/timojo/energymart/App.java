25d7e0405f36f22cc5f40d6bb3bdd1ef5a54d74e
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2020-10-08 13:57:59 +0300

    Added help text file

--- JavaPerformanceTuning/application/src/main/java/com/timojo/energymart/App.java
+++ JavaPerformanceTuning/application/src/main/java/com/timojo/energymart/App.java
@@ -2,16 +2,7 @@ package com.timojo.energymart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//http://localhost:8080/readings/read/
-//http://localhost:8080/plans/recommend/
-//CMD open jconsole --> jconsole folder MBeans/Metrics
-//CMD open jmc Java mission control
-//jcmd change default value PID=18288 //jcmd 18288 VM.set_flag CMSWaitDuration 1500 //jinfo -flag CMSWaitDuration=1500 18288
-//jcmd 18288 VM.flags -all --> verify intx CMSWaitDuration = 1500 {manageable} {attach}
-//jcmd 18288 Thread.print --> Threads
-//Jmeter test CMD
-// jmeter -n -t energymart.jmx -l energymart.jtl -JThreads=100 -Jduration=120
-//jmeter -g energymart.jtl -o html-report (Generate html report)
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
